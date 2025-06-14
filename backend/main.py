7ae08ebc4919c014854521779b0219c7bd22b109
SAGANGER <sagang20000@gmail.com>
2025-06-14 13:12:45 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -232,5 +232,4 @@ async def analyze_wallets(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    logger.info("Starting server...")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
